447c5391686d110139effe25475cfd760bee9628
bigdirecta <theodorecaswell@outlook.com>
2024-09-13 18:59:23 +1000

    -baseurltest

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function loadContent(url) {
-    const baseUrl = "/";
+    const baseUrl = "https://theodorebulleid.net/";
     const fullUrl = url.startsWith("/") ? url : `${baseUrl}${url}`;
     console.log("Attempting to load content from:", fullUrl);
     fetch(fullUrl)
